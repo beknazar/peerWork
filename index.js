6e6b85b9d9ee284ec13ba2339cff3d86ea62a2cd
beknazar <beknazar23@gmail.com>
2015-07-20 17:16:41 +0900

    initial tempalate

--- index.js
+++ index.js
@@ -3,11 +3,16 @@ var stormpath = require ('express-stormpath');
 
 var app = express();
 
+app.use("/assets", express.static(__dirname + '/assets'));
+
 app.set('views', './views');
 app.set('view engine', 'jade');
 
-app.get('/' function (req, res) {
+app.get('/', function (req, res) {
 	res.render('home', {
-		title: 'peerWork'
+		title: 'peerWork',
+		description: 'increase your productivity by working peer to peer'
 	});
 });
+
+app.listen(3000);
